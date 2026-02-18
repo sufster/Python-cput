94b065729410d0d982ace48413da8c3c33d4b6a6
Yusuf <yusalie@gmail.com>
2026-02-18 12:36:36 +0200

    excercise 1 and 2 added

---
+++ exercise1.py
@@ -0,0 +1,7 @@
+lion = "The lion catches "
+
+springbok = "the springbox"
+
+print(lion + springbok)
+
+print(lion +"\n" +springbok)
---
+++ exercise2.py
@@ -0,0 +1,7 @@
+itemPrice = 100
+
+vat = 14/100
+
+finalPrice = itemPrice+(itemPrice*vat)
+
+print(finalPrice)
