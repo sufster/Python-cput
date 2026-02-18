6aa2c7c21afe376107aa34ac230c857db8008498
Yusuf <yusalie@gmail.com>
2026-02-18 12:19:02 +0200

    first commit

---
+++ Calc.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter your first number: "))
+
+num2 = int(input("Enter your second number: "))
+
+result = num1 + num2
+
+print(str(num1) + " + " + str(num2) + " = " + str(result))
